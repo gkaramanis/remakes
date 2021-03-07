5c086d5b2e84be70e6b4fb33c3c5db55853108db
Georgios Karamanis <georgios.karamanis@me.com>
2021-03-07 11:38:52 +0100

    use 4-siffrig SSYK 2012

--- yrken2019/yrken2014_2019.R
+++ yrken2019/yrken2014_2019.R
@@ -3,8 +3,8 @@ library(hablar)
 library(ggbump)
 library(colorspace)
 
-yrken2014_2018 <- read_csv(here::here("yrken2019", "data", "yrken2014_2018.csv"))
-yrken2019 <- read_csv(here::here("yrken2019", "data", "yrken2019.csv"))
+yrken2014_2018 <- read_csv(here::here("yrken2019", "data", "yrken2014_2018_4.csv"))
+yrken2019 <- read_csv(here::here("yrken2019", "data", "yrken2019_4.csv"))
 yrken <- yrken2014_2018 %>% 
   left_join(yrken2019)
 
@@ -27,27 +27,27 @@ pal <- c("#a3b267",
          "#ff892c",
          "#004291",
          "#ff5147",
+         "#7f000f",
          "#b7a1db",
          "#ae5900",
          "#641969",
          "#4e3f00",
          "#ff4fb6",
-         "#7f000f",
          "#f587c2",
          "#c70061",
          "#a9626a")
 
 ggplot(yrken_topp) +
-  geom_bump(aes(x = year, y = rank, group = yrke, color = yrke), size = 1.5) +
-  geom_point(aes(x = year, y = rank, color = yrke), size = 2) +
+  geom_bump(aes(x = year, y = rank, group = yrke, color = yrke), size = 1.75) +
+  geom_point(aes(x = year, y = rank, color = yrke), size = 2.5) +
   geom_text(data = yrken_topp %>% filter(year == 2019), aes(x = 2019, y = rank, label = paste0(1:15, ". ", str_wrap(yrke, 25)), color = yrke), hjust = 0, nudge_x = 0.1, family = "October Condensed Tamil Medium", lineheight = 0.9, size = 3.8) +
-  scale_y_reverse(breaks = 1:15) +
+  scale_y_reverse() +
   scale_x_continuous(limits = c(2014, 2020), breaks = 2014:2019) +
   scale_color_manual(values = pal) +
   coord_cartesian(clip = "off") +
   labs(
     title = "De 15 vanligaste yrkena för kvinnor, 2014-2019",
-    subtitle = "Baserat på antal anställda 16-64 år, yrken enligt SSYK 2012",
+    subtitle = "Efter antal anställda 16-64 år, yrken enligt 4-siffrig SSYK 2012",
     caption = "Källa: SCB · Grafik: Georgios Karamanis"
     ) +
   theme_void(base_family = "DIN Condensed Bold") +
@@ -55,9 +55,9 @@ ggplot(yrken_topp) +
     legend.position = "none",
     plot.background = element_rect(fill = "grey97", color = NA),
     axis.text.x = element_text(color = "grey40", size = 14),
-    plot.title = element_text(hjust = 0.5, size = 24),
-    plot.subtitle = element_text(hjust = 0.5, size = 18),
+    plot.title = element_text(hjust = 0.5, size = 24, color = "grey10"),
+    plot.subtitle = element_text(hjust = 0.5, size = 18, color = "grey20", margin = margin(5, 0, 20, 0)),
     plot.caption = element_text(hjust = 0.5, size = 12, color = "grey30", margin = margin(20, 0, 0, 0)),
     plot.margin = margin(20, 50, 20, 10)
   ) +
-  ggsave(here::here("yrken2019", "plots", "yrken2014_2019.png"), dpi = 320, height = 10, width = 8)
+  ggsave(here::here("yrken2019", "plots", "yrken2014_2019.png"), dpi = 320, height = 11, width = 8)
